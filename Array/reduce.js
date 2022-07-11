adf69b6e9b9277ecf386dcb326df733d21ed2c50
wuxianshi <1073846680@qq.com>
2022-07-11 18:20:37 +0800

    reduce

--- Array/reduce.js
+++ Array/reduce.js
@@ -7,17 +7,18 @@
   const array = Array.prototype.slice.apply(this);
   if(typeof reducer !== 'function') throw new TypeError('reducer is not a function');
   const hasInitial = arguments.length >= 2;
-  if(array.length == 0) {
+  const length = array.length;
+  if(length == 0) {
     if(!hasInitial) return new Error('Empty array with no initial value');
     return initialValue;
   }
-  if(array.length == 1 && !hasInitial) return array[0];
+  if(length == 1 && !hasInitial) return array[0];
   let index = 0;
   if(!hasInitial) {
-    initialValue = array[0];
-    index = 1;
+    initialValue = array[index];
+    index += 1;
   }
-  for(let i = index, length = array.length; i < length; i++) {
+  for(let i = index; i < length; i++) {
     initialValue = reducer(initialValue, array[i], i, array);
   }
   return initialValue;
